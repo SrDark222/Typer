a81e99a099c58ec104b08cb327a0f85aaabde100
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-05-20 01:07:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
+
+let currentText = '';
+let i = 0;
+
 function startTyping() {
-  const text = document.getElementById('userText').value;
+  currentText = document.getElementById('userText').value;
   const typingText = document.getElementById('typingText');
   typingText.innerHTML = '';
-  let i = 0;
-
-  function type() {
-    if (i < text.length) {
-      typingText.innerHTML += text.charAt(i);
-      i++;
-      setTimeout(type, 100);
-    }
-  }
-
+  i = 0;
   type();
+}
+
+function type() {
+  if (i < currentText.length) {
+    document.getElementById('typingText').innerHTML += currentText.charAt(i);
+    i++;
+    setTimeout(type, 100);
+  }
+}
+
+function gerarGIF() {
+  const element = document.getElementById('typingBox');
+  const gif = new GIF({
+    workers: 2,
+    quality: 10,
+    workerScript: 'https://cdnjs.cloudflare.com/ajax/libs/gif.js/0.2.0/gif.worker.js'
+  });
+
+  html2canvas(element).then(canvas => {
+    gif.addFrame(canvas, {delay: 200});
+    gif.on('finished', function(blob) {
+      window.open(URL.createObjectURL(blob));
+    });
+    gif.render();
+  });
+}
+
+: 200});
+    gif.on('finished', function(blob) {
+      const url = URL.createObjectURL(blob);
+      document.getElementById('loading').style.display = 'none';
+      window.open(url, '_blank');
+    });
+    gif.render();
+  });
 }
