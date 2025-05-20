41be2384fe89c4d649e24b31e2a79a1ceff00fc8
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-05-20 01:10:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,27 @@
 let currentText = '';
 let i = 0;
 
-function startTyping() {
-  currentText = document.getElementById('userText').value;
+function startProcess() {
+  currentText = document.getElementById('userText').value.trim();
+  if (!currentText) {
+    alert("Preencha o campo antes de gerar!");
+    return;
+  }
+
   const typingText = document.getElementById('typingText');
   typingText.innerHTML = '';
   i = 0;
-  type();
+  document.getElementById('loading').style.display = 'block';
+  typeAndGenerate();
 }
 
-function type() {
+function typeAndGenerate() {
   if (i < currentText.length) {
     document.getElementById('typingText').innerHTML += currentText.charAt(i);
     i++;
-    setTimeout(type, 100);
+    setTimeout(typeAndGenerate, 100);
+  } else {
+    gerarGIF();
   }
 }
 
@@ -29,17 +37,12 @@ function gerarGIF() {
   html2canvas(element).then(canvas => {
     gif.addFrame(canvas, {delay: 200});
     gif.on('finished', function(blob) {
-      window.open(URL.createObjectURL(blob));
-    });
-    gif.render();
-  });
-}
-
-: 200});
-    gif.on('finished', function(blob) {
       const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "typer_dk.gif";
+      a.click();
       document.getElementById('loading').style.display = 'none';
-      window.open(url, '_blank');
     });
     gif.render();
   });
